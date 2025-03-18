336f8408c660b6c05aa8ff0f1975fe14b53de11f
Shuddae <monesatshiu@gmail.com>
2025-03-18 13:01:29 +0200

    new week

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -2,14 +2,16 @@ using System;
 
 class Program
 {
-
-     static Journal journal = new Journal();
+    
+    static Journal journal = new Journal();
     static PromptGenerator generator = new PromptGenerator();
-        static void Main(string[] args)
+
+
+    static void Main(string[] args)
     {
         while (true)
         {
-             Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display all entries");
             Console.WriteLine("3. Save to file");
             Console.WriteLine("4. Load from file");
@@ -17,12 +19,10 @@ class Program
             Console.Write("Choose an option: ");
             string option = Console.ReadLine();
         }
-        
-    }
-
+    } 
     public void SaveToFile(string file)
     {
-         Console.Write("Enter filename: ");
+        Console.Write("Enter filename: ");
         string filename = Console.ReadLine();
     }
 
@@ -31,4 +31,6 @@ class Program
         Console.Write("Enter filename: ");
         string filename = Console.ReadLine();
     }
+    
+
 }
