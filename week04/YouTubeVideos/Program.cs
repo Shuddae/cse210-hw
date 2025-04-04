85b92260e7c416a2a0adfcf47c6599d983a50aa9
Shuddae <monesatshiu@gmail.com>
2025-04-04 14:17:44 +0200

    new video

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-          Video video = new Video("My Video", "John Doe", 300);
+          Video video = new Video("My Video", "Will Smith", 300);
 
-        Comment comment1 = new Comment("Jane Doe", "Great video!");
-        Comment comment2 = new Comment("Bob Smith", "Excellent content!");
+        Comment comment1 = new Comment("Antonio Barendes", "Great video!");
+        Comment comment2 = new Comment("Michael Jackson", "Excellent content!");
 
         video.AddComment(comment1);
         video.AddComment(comment2);
